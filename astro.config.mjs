546671a9706fdbaac7c8828ed4c6ddc0703c6674
f24816 <ilikeorange121@gmail.com>
2024-08-15 19:12:36 +0300

    sort by date and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,25 @@ import tailwind from "@astrojs/tailwind";
 import pagefind from "astro-pagefind";
 import vercel from '@astrojs/vercel/serverless';
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "hybrid",
-    site: "https://f24816.vercel.app/",
-    adapter: vercel({
-        webAnalytics: { enabled: true }
-    }),
-    integrations: [
-        tailwind({
-            // Disable injecting a basic `base.css` import on every page.
-            applyBaseStyles: false,
-        }),
-        pagefind()
-    ],
-    image: {
-        // This lets astro:assets work with remote images
-        remotePatterns: [{ protocol: "https" }],
+  output: "hybrid",
+  site: "https://f24816.vercel.app/",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
     }
+  }),
+  integrations: [tailwind({
+    // Disable injecting a basic `base.css` import on every page.
+    applyBaseStyles: false
+  }), pagefind(), robotsTxt()],
+  image: {
+    // This lets astro:assets work with remote images
+    remotePatterns: [{
+      protocol: "https"
+    }]
+  }
 });
